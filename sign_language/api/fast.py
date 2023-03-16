1b740638019bb02107cd63ebb0a85297dc9ccb28
stuart howarth <stuarthowarth88@gmail.com>
2023-03-16 19:05:02 +0000

    api running locally, still needs docorising

--- sign_language/api/fast.py
+++ sign_language/api/fast.py
@@ -1,29 +1,28 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import Response
-
-import numpy as np
+from fastapi import FastAPI
+from keras.models import load_model
+import os
 import cv2
-
+import numpy as np
+import tensorflow as tf
 
 app = FastAPI()
 
+
+app.state.model = load_model(os.environ['MODEL'])
+
+
+img = cv2.imread('/home/stuart/code/stuhow/sign_language_translation/processed_images/all_images/A/Image_A485.jpg')
 
 @app.get("/")
 def index():
     return {"status": "ok"}
 
-@app.post('/upload_image')
-async def receive_image(img: UploadFile=File(...)):
-    ### Receiving and decoding the image
-    contents = await img.read()
-
-    nparr = np.fromstring(contents, np.uint8)
-    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
-
-    ### Do cool stuff with your image.... For example face detection
-    annotated_img = annotate_face(cv2_img)
-
-    ### Encoding and responding with the image
-    im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
-    return Response(content=im.tobytes(), media_type="image/png")
+@app.get('/predict')
+def predict():
+    prediction_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") + ["del", "space"]
+    cropped_image = img.reshape(1, 56, 56, 3)
+    prediction = app.state.model.predict(cropped_image)
+    letter = np.argmax(prediction[0], axis=0)
+    print(prediction)
+    print(letter)
+    return {'prediction': prediction_list[letter]}
